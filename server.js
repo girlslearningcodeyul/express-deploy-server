c04d256fbe38607ec9f1a5eeb1ba667453fab799
girlslearningcodeyul <ksenia@ladieslearningcode.com>
2018-05-01 14:38:01 -0400

    fourth commit

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 var express = require('express')
 var app = express()
 app.get ('/', (req, res) => {
-    res.send("Javascript rocks!");
+    res.send("Javascript boulders!");
 })
 app.listen(3001);
